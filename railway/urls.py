1d6a2f9f8ecba5af172748171611c33e31a44b61
TYordanova <tanyaiyord@gmail.com>
2023-10-12 11:21:55 +0300

    Implemented functionality for station and train  addition. Made minor changes in edition functionalities of train and station.

--- railway/urls.py
+++ railway/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path('live_schedule/', views.live_schedule_departing, name='live_schedules'),
     path('live_schedule/arrivals/', views.live_schedule_arrivals, name='live_schedule_arrivals'),
     path('book_ticket/', views.book_ticket, name='book_ticket'),
-    path('user_registration/', views.user_registration, name='user_registration'),
-    path('accounts/login/', views.user_login, name='user_login'),
-    path('user_logout/', views.user_logout, name='user_logout'),
     path('edit_station/<int:station_id>/', views.edit_station, name='edit_station'),
-    path('edit_train/<int:train_id>/', views.edit_train, name='edit_train')
+    path('add_station/', views.add_station, name='add_station'),
+    path('delete_station/<int:pk>/', views.delete_station, name='delete_station'),
+    path('edit_train/<int:train_id>/', views.edit_train, name='edit_train'),
+    path('add_train/', views.add_train, name='add_train'),
+    path('delete_train/<int:id>/', views.delete_train, name='delete_train')
 ]
--- railway/views.py
+++ railway/views.py
@@ -1,12 +1,9 @@
 """ Views for Railway system."""
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from .models import Station, Train, Route, Ticket
-from .forms import StationForm, TrainForm, BookTicketForm, UserRegisterForm
+from .forms import StationForm, TrainForm, BookTicketForm
 from django.utils import timezone
 import random
 
@@ -74,7 +71,6 @@ def live_schedule_arrivals(request):
     })
 
 
-@login_required
 def book_ticket(request):
     """ Book a ticket view. It checks if the user is authenticated. If not - the user is redirected to the login page.
         Else the user can book a ticket.
@@ -83,101 +79,74 @@ def book_ticket(request):
                Returns:
         """
 
-    if request.user.is_authenticated:
-        ticket_form = BookTicketForm()
-        context = {
-            'ticket_form': ticket_form
-        }
+    context = {}
 
-        if request.method == 'POST':
-            ticket_form = BookTicketForm(request.POST)
+    if request.method == 'POST':
+        ticket_form = BookTicketForm(request.POST)
 
-            if ticket_form.is_valid():
+        if ticket_form.is_valid():
 
-                seat_number = ticket_form.cleaned_data.get('seat_number')
-                ticket_line = ticket_form.cleaned_data.get('ticket_line')
+            seat_number = ticket_form.cleaned_data.get('seat_number')
+            ticket_line = ticket_form.cleaned_data.get('ticket_line')
 
-                max_quantity_of_tickets = ticket_line.quantity
+            max_quantity_of_tickets = ticket_line.quantity
 
-                if max_quantity_of_tickets == 0:
-                    context['max_quantity']=True
-                    return render(request, 'railway/book_ticket.html', context=context)
+            if max_quantity_of_tickets == 0:
+                context['max_quantity'] = True
+                return render(request, 'railway/book_ticket.html', context=context)
 
-                ticket_form.save()
-                context['success'] = True
+            ticket_form.save()
+            context['success'] = True
 
-                max_quantity_of_tickets -= 1
-                ticket_line.quantity = max_quantity_of_tickets
-                ticket_number=Ticket().ticket_number
-
+            max_quantity_of_tickets -= 1
+            ticket_line.quantity = max_quantity_of_tickets
+            ticket_number = Ticket().ticket_number
+            if request.user.is_authenticated:
                 ticket_line.save()
 
                 context['ticket'] = ticket_line
-                context['seat_number']=seat_number
-                context['ticket_number']=ticket_number
+                context['seat_number'] = seat_number
+                context['ticket_number'] = ticket_number
 
                 return render(request, 'railway/book_ticket.html', context=context)
 
-            else:
-                context['ticket_errors']=True
-                return render(request, 'railway/seat_taken.html', context=context)
         else:
-            departing_station_get = request.GET.get('departing_station_name')
-            arrival_station_get = request.GET.get('arrival_station_name')
-            date = request.GET.get('departure_time')
+            context['ticket_errors'] = True
+            return render(request, 'railway/seat_taken.html', context=context)
+    else:
+        departing_station_get = request.GET.get('departing_station_name')
+        arrival_station_get = request.GET.get('arrival_station_name')
+        date = request.GET.get('departure_time')
 
-            ticket_form = BookTicketForm(arrival_station_name=arrival_station_get,
-                                         departing_station_name=departing_station_get,
-                                         departure_time=date)
+        ticket_form = BookTicketForm(arrival_station_name=arrival_station_get,
+                                     departing_station_name=departing_station_get,
+                                     departure_time=date)
 
         return render(request, 'railway/book_ticket.html', {
             'form': ticket_form
         })
+
+
+def add_station(request):
+    """ Add station
+        Args:
+            request: Http request
+        Returns:
+    """
+    context = {}
+    if request.POST:
+        station_form = StationForm(request.POST)
+        if station_form.is_valid():
+            new_station_name = station_form.cleaned_data['name']
+            new_station = Station(name=new_station_name)
+            new_station.save()
+            context['form'] = station_form
+            context['success'] = True
+        return render(request, 'railway/add_station.html', context)
     else:
-        return render(request, 'railway/user_login.html')
-
-
-def user_registration(request, form=None):
-    """This function is used for user registration. """
-    if request.method == 'POST':
-        register_form = UserRegisterForm(request.POST)
-        if register_form.is_valid():
-            register_form.save()
-            username = register_form.cleaned_data['username']
-            password1 = register_form.cleaned_data['password1']
-            password2 = register_form.cleaned_data['password2']
-            user = authenticate(username=username, password=password1)
-            login(request, user)
-            messages.success(request, 'Congrats! You registration was successful.')
-            return redirect('index')
-    else:
-        register_form = UserRegisterForm()
-    return render(request, 'railway/user_registration.html', {'form': register_form,
-                                                              'success': True})
-
-
-def user_login(request):
-    """This function is used for logging in user. """
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-
-            login(request, user)
-            return redirect('index')
-        else:
-            messages.error(request, 'Something went wrong when logging in. Please try again')
-            return render(request, 'railway/user_login.html', {'success': False})
-    else:
-        return render(request, 'railway/user_login.html')
-
-
-def user_logout(request):
-    logout(request)
-    messages.success(request, 'You logged out.')
-    return redirect('index')
+        station_form = StationForm()
+        context['form'] = station_form
+    return render(request, 'railway/add_station.html', context)
 
 
 def edit_station(request, station_id):
@@ -187,22 +156,58 @@ def edit_station(request, station_id):
             station_id: Station id
         Returns:
     """
+    context = {}
     if request.method == 'POST':
         station = Station.objects.get(pk=station_id)
-        edit_form = StationForm(request.POST, instance=station)
-        if edit_form.is_valid():
-            edit_form.save()
-
-            return render(request, 'railway/edit_station.html', {
-                'form': StationForm(),
-                'success': True,
-            })
-    else:
+        station_edit_form = StationForm(request.POST, instance=station)
+        if station_edit_form.is_valid():
+            station_edit_form.save()
+            context['form'] = station_edit_form
+            return redirect('index')
+        else:  # not valid form
+            context['form'] = station_edit_form
+    else:  # GET request
         station = Station.objects.get(pk=station_id)
-        edit_form = StationForm(instance=station)
-        return render(request, 'railway/edit_station.html', {
-            'form': StationForm(),
-        })
+        station_edit_form = StationForm(instance=station)
+        context['form'] = station_edit_form
+    return render(request, 'railway/edit_station.html', context)
+
+
+def delete_station(request, pk):
+    """ Delete station
+            Args:
+                request: Http request
+                station_id: Station id
+            Returns:
+        """
+
+    if request.method == 'POST':
+        station = Station.objects.get(pk=pk)
+        station.delete()
+    return redirect('index')
+
+
+def add_train(request):
+    """ Add station
+        Args:
+            request: Http request
+        Returns:
+    """
+    context = {}
+    if request.POST:
+        train_form = TrainForm(request.POST)
+        if train_form.is_valid():
+            new_train_name = train_form.cleaned_data['name']
+            train_type_select = train_form.cleaned_data['train_type']
+            new_station = Train(name=new_train_name, train_type=train_type_select)
+            new_station.save()
+            context['form'] = train_form
+            context['success'] = True
+        return render(request, 'railway/add_train.html', context)
+    else:
+        train_form = TrainForm()
+        context['form'] = train_form
+    return render(request, 'railway/add_train.html', context)
 
 
 def edit_train(request, train_id):
@@ -212,19 +217,32 @@ def edit_train(request, train_id):
             train_id: Train id
         Returns:
     """
+    context = {}
     if request.method == 'POST':
         train = Train.objects.get(pk=train_id)
-        edit_form = TrainForm(request.POST, instance=train)
-        if edit_form.is_valid():
-            edit_form.save()
-
-            return render(request, 'railway/edit_train.html', {
-                'form': TrainForm(),
-                'success': True,
-            })
-    else:
+        train_edit_form = TrainForm(request.POST, instance=train)
+        if train_edit_form.is_valid():
+            train_edit_form.save()
+            context['form'] = train_edit_form
+            return redirect('index')
+        else:  # not valid form
+            context['form'] = train_edit_form
+    else:  # GET request
         train = Train.objects.get(pk=train_id)
-        edit_form = TrainForm(instance=train)
-        return render(request, 'railway/edit_train.html', {
-            'form': TrainForm(),
-        })
+        train_edit_form = TrainForm(instance=train)
+        context['form'] = train_edit_form
+    return render(request, 'railway/edit_train.html', context)
+
+
+def delete_train(request, id):
+    """ Delete train
+            Args:
+                request: Http request
+                id: Train id
+            Returns:
+        """
+
+    if request.method == 'POST':
+        train = Train.objects.get(pk=id)
+        train.delete()
+    return redirect('index')
